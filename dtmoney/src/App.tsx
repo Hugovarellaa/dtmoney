ef3d09139f6e7b4f344e046a0b3a89cc20b6dac6
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-05-22 19:15:22 -0300

    Estrutura da aplicação
    Configurando estrutura

---
+++ dtmoney/src/App.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyled } from "./styles/global";
+
+export function App() {
+  return (
+    <div className="App">
+
+
+     <h1>Hello World</h1>
+     <GlobalStyled/>
+    </div>
+  );
+}
+
---
+++ dtmoney/src/styles/global.ts
@@ -0,0 +1,54 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyled = createGlobalStyle`
+    :root{
+        --background: #f0f2f5;
+        --red: #E52E4D;
+        --blue: #5429cc;
+        --blue-light: #6933ff;
+        --text-title: #363f5f;
+        --text-body: #969cb3;
+        --shape:#FFFFFF;
+        --green: #33CC95;
+    }
+    
+    
+    *{
+        margin:0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    //font-size: 16px(Desktop) Padrão
+    html{
+        @media(max-width:1080px){
+            font-size:93.75%; //15px
+        }
+        @media(max-width:720px){
+            font-size: 87.5%; //14px
+        }
+    }
+
+    body{
+        background-color: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button{
+        font-family:'Poppins', sans-serif;
+        font-weight: 400;
+    }
+    h1, h2, h3, h4, h5,h6, strong{
+        font-weight: 600;
+    }
+
+    button{
+        cursor: pointer;
+    }
+
+    [disabled]{
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+
+`
