e08f36636354c900b432effaad983437c8c42739
hugovarellaa <hugovarellaa@gmail.com>
2022-12-16 14:15:36 -0300

    Create Transactiontable component

---
+++ dt-money/src/components/TransactionTable/index.tsx
@@ -0,0 +1,7 @@
+import { TransactionTableContainer } from './styles'
+
+export function TransactionTable() {
+  return (
+    <TransactionTableContainer>Transaciton Table</TransactionTableContainer>
+  )
+}
