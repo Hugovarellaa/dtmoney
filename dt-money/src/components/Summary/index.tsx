87797dc58351f3a11667f7497f142485cefa4d0a
hugovarellaa <hugovarellaa@gmail.com>
2022-12-16 12:01:50 -0300

    Create Summary component

---
+++ dt-money/src/components/Summary/index.tsx
@@ -0,0 +1,11 @@
+import { SummaryContainer } from './styles'
+
+export function Summary() {
+  return (
+    <SummaryContainer>
+      <h1>
+        <Summary></Summary>
+      </h1>
+    </SummaryContainer>
+  )
+}
