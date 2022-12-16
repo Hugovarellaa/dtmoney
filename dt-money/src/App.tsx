f575f63e2452757d316d12c3c781a5070ccab335
hugovarellaa <hugovarellaa@gmail.com>
2022-12-16 17:47:28 -0300

    update

--- dt-money/src/App.tsx
+++ dt-money/src/App.tsx
@@ -1,11 +1,6 @@
 import { ThemeProvider } from 'styled-components'
 import { Home } from './Pages/Home'
-import { makeServer } from './services/mirage'
 import { defaultTheme, GlobalStyle } from './styles'
-
-if (process.env.NODE_ENV !== 'production') {
-  makeServer()
-}
 
 export function App() {
   return (
--- dt-money/src/components/Header/index.tsx
+++ dt-money/src/components/Header/index.tsx
@@ -1,33 +1,12 @@
-import { useState } from 'react'
-import Modal from 'react-modal'
 import { Logo } from '../Logo'
 import { ButtonNewTransaction, HeaderContainer, HeaderContent } from './styles'
 
 export function Header() {
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
-    useState(false)
-
-  function handleOpenModal() {
-    setIsNewTransactionModalOpen(true)
-  }
-
-  function handleCloseModal() {
-    setIsNewTransactionModalOpen(false)
-  }
-
   return (
     <HeaderContainer>
-      <Modal
-        isOpen={isNewTransactionModalOpen}
-        onRequestClose={handleCloseModal}
-      >
-        <h2>Nova Transação</h2>
-      </Modal>
       <HeaderContent>
         <Logo />
-        <ButtonNewTransaction onClick={handleOpenModal}>
-          Nova transação
-        </ButtonNewTransaction>
+        <ButtonNewTransaction>Nova transação</ButtonNewTransaction>
       </HeaderContent>
     </HeaderContainer>
   )
