0c50e932e12623054b3783a9c0d9b49258fb6895
hugovarellaa <hugovarellaa@gmail.com>
2022-12-16 14:13:37 -0300

    Summary Layout ok

--- dt-money/src/components/Summary/index.tsx
+++ dt-money/src/components/Summary/index.tsx
@@ -1,9 +1,18 @@
 import { SummaryContainer } from './styles'
 
+import { Transactions } from './components/Transactions/transactions'
+
 export function Summary() {
   return (
     <SummaryContainer>
-      <h1>Summary</h1>
+      <Transactions title="Entradas" type="deposit" value="R$ 12.000,00" />
+      <Transactions title="Saídas" type="withdraw" value="R$ 12.000,00" />
+      <Transactions
+        title="Total"
+        type="total"
+        value="R$ 12.000,00"
+        lastTransactions
+      />
     </SummaryContainer>
   )
 }
