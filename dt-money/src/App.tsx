5dc2c542196c2a7fb022839ec584b4eb527c1057
hugovarellaa <hugovarellaa@gmail.com>
2022-12-16 11:22:06 -0300

    GlobalStyles

--- dt-money/src/App.tsx
+++ dt-money/src/App.tsx
@@ -1,7 +1,11 @@
+import { Home } from './Pages/Home'
+import { GlobalStyle } from './styles'
+
 export function App() {
   return (
-    <div>
-      <h1>Ola mundo</h1>
-    </div>
+    <>
+      <Home />
+      <GlobalStyle />
+    </>
   )
 }
---
+++ dt-money/src/styles/global/GlobalStyles.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  html {
+    @media(max-width:1080px){
+      font-size: 93.75%;
+    }
+    @media(max-width:720px){
+      font-size: 87.5%
+    }
+  }
+
+  :root{}
+
+  body {
+    background-color: black ;
+    color: white;
+  }
+  body, input, button {
+    font-family: sans-serif;
+    font-size: 1rem;
+    line-height: 1.4;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  [disabled]{
+    pointer-events: none;
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`
