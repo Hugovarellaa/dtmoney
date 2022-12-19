7b56e94a5f2fa5333fd0ab2d1d34b61d09203760
hugovarellaa <hugovarellaa@gmail.com>
2022-12-19 13:44:24 -0300

    Modal open and close

--- web/src/shared/components/TransactionModal/index.tsx
+++ web/src/shared/components/TransactionModal/index.tsx
@@ -1,9 +1,14 @@
+import Modal from 'react-modal'
+import { useModal } from '../../context/useModal'
 import { TransactionModalContainer } from './styles'
 
 export function TransactionModal() {
+  const { modalIsOpen, closeModal } = useModal()
   return (
-    <TransactionModalContainer>
-      <h1>Modal</h1>
-    </TransactionModalContainer>
+    <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
+      <TransactionModalContainer>
+        <h2>Modal aberto</h2>
+      </TransactionModalContainer>
+    </Modal>
   )
 }
