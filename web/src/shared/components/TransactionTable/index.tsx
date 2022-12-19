83a764e2ceb5faa080d926500e34eb59e2f60e30
hugovarellaa <hugovarellaa@gmail.com>
2022-12-19 11:47:58 -0300

    Component TransactionTable

---
+++ web/src/shared/components/TransactionTable/index.tsx
@@ -0,0 +1,9 @@
+import { TransactionTableContainer } from './styles'
+
+export function TransactionTable() {
+  return (
+    <TransactionTableContainer>
+      <h1>TransactionTable</h1>
+    </TransactionTableContainer>
+  )
+}
---
+++ web/src/shared/components/TransactionTable/styles.ts
@@ -0,0 +1,3 @@
+import styled from 'styled-components'
+
+export const TransactionTableContainer = styled.div``
