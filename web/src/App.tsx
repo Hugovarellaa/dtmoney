d8516485243367f1e8b75ce54734cf0f7d5476d5
hugovarellaa <hugovarellaa@gmail.com>
2022-12-19 12:08:41 -0300

    Config api faker miragejs

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,11 @@
 import { ThemeProvider } from 'styled-components'
 import { Home } from './pages/Home'
+import { makeServer } from './shared/services/mirage'
 import { defaultTheme } from './shared/styles/theme/default'
 
+if (process.env.NODE_ENV !== 'production') {
+  makeServer()
+}
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
