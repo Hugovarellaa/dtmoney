0d38a5ccaba5ee7f91dcd39608b2d343fed27847
hugovarellaa <hugovarellaa@gmail.com>
2022-12-19 13:45:53 -0300

    Modal in grid

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import { Dashboard } from '../../shared/components/Dashboard'
 import { Header } from '../../shared/components/Header'
+import { TransactionModal } from '../../shared/components/TransactionModal'
 import { GlobalStyles } from '../../shared/styles/global/GlobalStyles'
 
 export function Home() {
@@ -8,6 +9,7 @@ export function Home() {
       <Header />
       <Dashboard />
 
+      <TransactionModal />
       <GlobalStyles />
     </div>
   )
