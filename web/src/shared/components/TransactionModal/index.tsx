dbc5ab3724c315f0053089a63646bfff53848cfa
hugovarellaa <hugovarellaa@gmail.com>
2022-12-19 13:41:42 -0300

    Component modal

---
+++ web/src/shared/components/TransactionModal/index.tsx
@@ -0,0 +1,9 @@
+import { TransactionModalContainer } from './styles'
+
+export function TransactionModal() {
+  return (
+    <TransactionModalContainer>
+      <h1>Modal</h1>
+    </TransactionModalContainer>
+  )
+}
---
+++ web/src/shared/components/TransactionModal/styles.ts
@@ -0,0 +1,3 @@
+import styled from 'styled-components'
+
+export const TransactionModalContainer = styled.div``
