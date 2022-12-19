09c41195fb1fb844ae57e4ab465f9593231fa707
hugovarellaa <hugovarellaa@gmail.com>
2022-12-19 20:11:02 -0300

    GlobalStyles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { Home } from "./pages/Home";
+import { GlobalStyles } from "./shared/styles/global/GlobalStyles";
 import defaultTheme from "./shared/styles/theme/defaultTheme";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Home />
+      <GlobalStyles />
     </ThemeProvider>
   );
 }
--- web/src/shared/styles/global/GlobalStyles.ts
+++ web/src/shared/styles/global/GlobalStyles.ts
@@ -1,3 +1,51 @@
 import { createGlobalStyle } from "styled-components";
 
-export const GlobalStyles = createGlobalStyle``;
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  html {
+    @media(max-width: 1080px){
+      font-size: 93.75%;
+    }
+    @media(max-width: 1080px){
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors.text};
+  }
+
+  body, input, select, textarea {
+    font-family: "Poppins" , sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+
+
+`;
