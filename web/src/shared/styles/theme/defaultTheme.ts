9d7b775e7d796bbf845d3a2415f05756e82c1c1f
hugovarellaa <hugovarellaa@gmail.com>
2022-12-19 20:02:49 -0300

    colors

--- web/src/shared/styles/theme/defaultTheme.ts
+++ web/src/shared/styles/theme/defaultTheme.ts
@@ -1,1 +1,15 @@
-export default {};
+export default {
+  colors: {
+    blue: "#5429CC",
+    "blue-light": "#6933FF",
+    green: "#33CC95",
+    red: "#E52E4D",
+
+    shape: "#FFFFFF",
+    title: "#363F5F",
+    text: "#969CB2",
+    background: "#F0F2F5",
+    input: "#E7E9EE",
+    border: "#D7D7D7",
+  },
+};
