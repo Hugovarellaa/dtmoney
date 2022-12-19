57df1b27a2d650f915ac77e71f8a10a165d622fb
hugovarellaa <hugovarellaa@gmail.com>
2022-12-19 10:38:14 -0300

    colors

--- web/src/shared/styles/theme/default.ts
+++ web/src/shared/styles/theme/default.ts
@@ -1,1 +1,12 @@
-export default {}
+export const defaultTheme = {
+  colors: {
+    blue: '#5429CC',
+    'blue-ligh': '#6933FF',
+    red: '#E52E4D',
+    green: '#33CC95',
+    shape: '#FFFFFF',
+    title: '#363F5F',
+    text: '#969CB2',
+    background: '#969CB2',
+  },
+} as const
