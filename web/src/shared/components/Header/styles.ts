512e0b8671683dbd13f5039c17ad79d94b1e9708
hugovarellaa <hugovarellaa@gmail.com>
2022-12-19 11:04:13 -0300

    hover button

--- web/src/shared/components/Header/styles.ts
+++ web/src/shared/components/Header/styles.ts
@@ -24,4 +24,10 @@ export const ButtonNewTransaction = styled.button`
   border: 0;
   padding: 0 2rem;
   height: 3rem;
+
+  transition: filter 0.2s;
+
+  &:hover {
+    filter: brightness(0.9);
+  }
 `
