78d7223ef5ad487f80829587c5e2ca95551093a3
hugovarellaa <hugovarellaa@gmail.com>
2022-12-19 21:25:03 -0300

    Transaction table

--- web/src/shared/components/TransactionTable/index.tsx
+++ web/src/shared/components/TransactionTable/index.tsx
@@ -1,9 +1,41 @@
-import { TransactionTableContainer } from './styles'
+import {
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  TransactionTableContainer,
+} from './styles'
 
 export function TransactionTable() {
   return (
     <TransactionTableContainer>
-      <h1>Table</h1>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Título</Th>
+            <Th>Valor</Th>
+            <Th>Categoria</Th>
+            <Th>Data</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>Desenvolvimento de website</Td>
+            <Td type="deposit">R$ 12.000,00</Td>
+            <Td>Dev</Td>
+            <Td>05/12/2022</Td>
+          </Tr>
+
+          <Tr>
+            <Td>Aluguel</Td>
+            <Td type="withdraw">- R$ 2.000,00</Td>
+            <Td>Casa</Td>
+            <Td>10/12/2022</Td>
+          </Tr>
+        </Tbody>
+      </Table>
     </TransactionTableContainer>
   )
 }
--- web/src/shared/components/TransactionTable/styles.ts
+++ web/src/shared/components/TransactionTable/styles.ts
@@ -1,3 +1,51 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const TransactionTableContainer = styled.div``
+interface Props {
+  type?: 'withdraw' | 'deposit'
+}
+
+export const TransactionTableContainer = styled.div`
+  margin-top: 4rem;
+`
+export const Table = styled.table`
+  width: 100%;
+  border-spacing: 0 0.5rem;
+`
+export const Thead = styled.thead``
+
+export const Th = styled.th`
+  font-size: 1rem;
+  color: ${({ theme }) => theme.colors.text};
+  line-height: 24px;
+  font-weight: 400;
+  text-align: left;
+  padding: 1rem 2rem;
+`
+export const Tr = styled.tr``
+
+export const Tbody = styled.tbody``
+
+export const Td = styled.td<Props>`
+  background-color: ${({ theme }) => theme.colors.shape};
+  color: ${({ theme }) => theme.colors.text};
+  padding: 1rem 2rem;
+  border-radius: 0.25rem;
+  font-size: 1rem;
+  line-height: 24px;
+  font-weight: 400;
+
+  &:first-child {
+    color: ${({ theme }) => theme.colors.title};
+  }
+
+  ${({ type }) =>
+    type === 'withdraw' &&
+    css`
+      color: ${({ theme }) => theme.colors.red};
+    `}
+  ${({ type }) =>
+    type === 'deposit' &&
+    css`
+      color: ${({ theme }) => theme.colors.green};
+    `}
+`
