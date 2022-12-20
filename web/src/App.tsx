4f0e534f6cc4b275c571afc050ab35a3a4312fb5
hugovarellaa <hugovarellaa@gmail.com>
2022-12-19 21:31:43 -0300

    React-modal config

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from 'styled-components'
 import { Home } from './pages/Home'
+import { ModalProvider } from './shared/context/useModal'
 import { GlobalStyles } from './shared/styles/global/GlobalStyles'
 import defaultTheme from './shared/styles/theme/defaultTheme'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Home />
+      <ModalProvider>
+        <Home />
+      </ModalProvider>
       <GlobalStyles />
     </ThemeProvider>
   )
---
+++ web/src/shared/context/useModal.tsx
@@ -0,0 +1,36 @@
+import { createContext, ReactNode, useState } from 'react'
+
+interface ModalContextData {
+  modalIsOpen: boolean
+  openModal: () => void
+  closeModal: () => void
+}
+
+interface ModalProviderProps {
+  children: ReactNode
+}
+
+const ModalContext = createContext({} as ModalContextData)
+
+export function ModalProvider({ children }: ModalProviderProps) {
+  const [modalIsOpen, setIsOpen] = useState(false)
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+  return (
+    <ModalContext.Provider
+      value={{
+        modalIsOpen,
+        openModal,
+        closeModal,
+      }}
+    >
+      {children}
+    </ModalContext.Provider>
+  )
+}
