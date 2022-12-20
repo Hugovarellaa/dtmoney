af744cb870cfd293443d726dbd8bc80aa3de2a5f
hugovarellaa <hugovarellaa@gmail.com>
2022-12-19 23:03:37 -0300

    Configs miragejs

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,10 +2,15 @@ import Modal from 'react-modal'
 import { ThemeProvider } from 'styled-components'
 import { Home } from './pages/Home'
 import { ModalProvider } from './shared/context/useModal'
+import { makeServer } from './shared/services/mirage'
 import { GlobalStyles } from './shared/styles/global/GlobalStyles'
 import defaultTheme from './shared/styles/theme/defaultTheme'
 
 Modal.setAppElement('#root')
+
+if (process.env.NODE_ENV !== 'production') {
+  makeServer()
+}
 
 export function App() {
   return (
---
+++ web/src/shared/services/mirage.ts
@@ -0,0 +1,23 @@
+import { createServer, Model } from 'miragejs'
+
+export function makeServer() {
+  const server = createServer({
+    models: {
+      transaction: Model,
+    },
+    routes() {
+      this.namespace = 'api'
+      this.timing = 750
+
+      this.get('/transactions', () => {
+        return this.schema.all('transaction')
+      })
+
+      this.post('/transactions', (schema, request) => {
+        const data = JSON.parse(request.requestBody)
+        return schema.create('transaction', data)
+      })
+    },
+  })
+  return server
+}
