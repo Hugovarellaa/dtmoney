64a41b670db5f8ec298d8bec457cf7162becfde2
hugovarellaa <hugovarellaa@gmail.com>
2022-12-19 21:05:14 -0300

    Layout dashboard

--- web/src/shared/components/Summary/components/Transaction/styles.ts
+++ web/src/shared/components/Summary/components/Transaction/styles.ts
@@ -1,11 +1,30 @@
 import styled from 'styled-components'
 
-export const TransactionContainer = styled.div``
+export const TransactionContainer = styled.div`
+  background-color: ${({ theme }) => theme.colors.shape};
+  padding: 1rem 2rem;
+  border-radius: 0.25rem;
+  color: ${({ theme }) => theme.colors.title};
+`
 
-export const TransactionHeader = styled.header``
+export const TransactionHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
 
-export const TransactionDetails = styled.p``
+export const TransactionDetails = styled.p`
+  font-size: 1rem;
+  color: ${({ theme }) => theme.colors.title};
+  line-height: 24px;
+  font-weight: 400;
+`
 
 export const TransactionImage = styled.img``
 
-export const TransactionAmount = styled.strong``
+export const TransactionAmount = styled.strong`
+  margin-top: 1.5rem;
+  font-size: 2rem;
+  font-weight: 500;
+  line-height: 3rem;
+`
